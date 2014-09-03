dbf77f1446a0307c373cf0d8ff18a896b0872e1a
Alex Hong <thealexhong@gmail.com>
2014-09-02 23:03:30 -0400

    initial commit

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 
-var myName = "Happy Birthday Desmond!";
+var myName = "HBD Desmond!";
 var letterColors = [red, orange, green, blue, purple];
 
-if(10 < 3) {
+//if(10 < 3) {
       bubbleShape = 'square';
-}
-else {
-      bubbleShape = 'circle';
-}
+//}
+//else {
+//      bubbleShape = 'circle';
+//}
 
 drawName(myName, letterColors);
 bounceBubbles();
